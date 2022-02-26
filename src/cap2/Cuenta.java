36f96151b6b3ab1b95f1f92be5500ae68199e791
rayggr <raymundo.gnz.rz@gmail.com>
2022-02-25 18:15:40 -0600

    probando

--- src/cap2/Cuenta.java
+++ src/cap2/Cuenta.java
@@ -2,6 +2,15 @@ package cap2;
 
 public class Cuenta {
 	private String nombre;
+	
+
+	public Cuenta() {
+		this("Sin nombre");
+	}
+	
+	public Cuenta(String nombre) {
+		this.nombre = nombre;
+	}
 
 	public void establecerNombre(String nombre) {
 		this.nombre = nombre;
--- src/cap2/PruebaCuenta.java
+++ src/cap2/PruebaCuenta.java
@@ -14,17 +14,9 @@ public class PruebaCuenta {
 		System.out.println();
 		
 		System.out.printf("El nombre en el objeto miCuenta es: %n%s%n",miCuenta.obtenerNombre());
-		System.out.println();
-		System.out.println("Test de scanner ingresando datos con espacio y metodo next");
-		
-		String inp1;
-		String inp2;
-		
-		System.out.print("Ingrese dos palabras separadas por espacios:");
-		inp1 = entrada.next();
-		inp2 = entrada.next();
-		
-		System.out.printf("El valor final de inp1 es %s y el de inp2 es %s",inp1,inp2);
+
 	}
 
 }
+
+
